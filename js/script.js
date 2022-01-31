4bd4119453b1b1018d99c75964b1549fc415c1a7
nixon2105 <radivilov1991@mail.ru>
2022-01-31 08:56:39 +0300

    feat: add randomData

--- js/script.js
+++ js/script.js
@@ -1,28 +1,33 @@
-const url = 'https://type.fit/api/quotes';
+const url = 'https://www.breakingbadapi.com/api/quotes';
 const btn = document.querySelector('.btn');
 const container = document.querySelector('.container__jokes');
 const jokesText = document.querySelector('.jokes__text');
 
 const createTemplate = (data) => {
-  // return `
-  //         <div class="text">${data.text}</div>
-  //         <div class="author">${data.author}</div>
-  // `;
+  return `
+          <div class="text">${data.quote}</div>
+          <div class="author">${data.author}</div>
+  `;
 };
 
 const getQuotes = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
+  // console.log(data);
   showQutes(data);
+  // console.log(shuffle(data));
 };
 
 // getQuotes(url);
 
 const showQutes = (data) => {
-  data.map((el) => {
-    // jokesText.innerHTML += createTemplate(el);
-    // console.log(el.text);
-  });
+  const randomData = Math.floor(Math.random() * data.length);
+  const randomQutes = data[randomData];
+  console.log(randomQutes);
+  // randomQutes.map((el) => {
+  //   // jokesText.innerHTML += createTemplate(el);
+  //   console.log(el.quote);
+  // });
 };
 
 btn.addEventListener('click', (e) => {
